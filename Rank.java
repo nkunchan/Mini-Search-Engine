105073770ad3946cb74c1c1056b35748815543cf
Nikita Kunchanwar <nikita.999.d@gmail.com>
2018-04-29 13:54:34 -0400

    Update Rank.java
    Formatted

--- Rank.java
+++ Rank.java
@@ -30,41 +30,25 @@ import org.apache.hadoop.mapreduce.lib.input.FileSplit;
 public class Rank extends Configured implements Tool {
 
    private static final Logger LOG = Logger .getLogger(Rank.class);
-public static void main( String[] args) throws  Exception {
+   public static void main( String[] args) throws  Exception {
       int res  = ToolRunner .run( new Rank(), args);
       System .exit(res);
    }
 
 
    public int run( String[] args) throws  Exception {
-
-     
-
-
-//conf.set("query", new String(sb.substring(0,sb.length()-1)));
- Job job  = Job .getInstance(getConf(), "job1");
-      job.setJarByClass( this .getClass());
-      
-
+      Job job  = Job .getInstance(getConf(), "job1");
+      job.setJarByClass( this .getClass()); 
       FileInputFormat.addInputPaths(job,  args[0]);
-
       FileOutputFormat.setOutputPath(job, new Path(args[1]));
       job.setMapperClass( Map .class);
       job.setReducerClass( Reduce .class);
-job.setMapOutputKeyClass(DoubleWritable.class);
-job.setMapOutputValueClass(Text.class);
-job.setSortComparatorClass(MyKeyComparator.class);
- //job.setSortComparatorClass(DoubleWritable.DecreasingComparator.class);
-
-
+      job.setMapOutputKeyClass(DoubleWritable.class);
+      job.setMapOutputValueClass(Text.class);
+      job.setSortComparatorClass(MyKeyComparator.class);
       job.setOutputKeyClass( Text .class);
       job.setOutputValueClass(DoubleWritable .class);
-//job.waitForCompletion(true);
-
-
-     return job.waitForCompletion( true)  ? 0 : 1;
-
-
+      return job.waitForCompletion( true)  ? 0 : 1;
    }
 public static class MyKeyComparator extends WritableComparator {
     protected MyKeyComparator() {
@@ -82,34 +66,16 @@ public static class MyKeyComparator extends WritableComparator {
 
    
    public static class Map extends Mapper<LongWritable, Text,  DoubleWritable,  Text > {
-    //  private final static IntWritable one  = new IntWritable( 1);
-    //  private Text word  = new Text();
-
       private static final Pattern WORD_BOUNDARY = Pattern .compile("\\s*\\B*\\s*");
-
       public void map(LongWritable offset, Text lineText, Context context)
         throws  IOException,  InterruptedException {
- //int  input_size= (int) context.getConfiguration().getLong("mapred.map.tasks",1);
-
-         String line  = lineText.toString();
+          String line  = lineText.toString();
                     String[] words=line.split("\\s+");
-if(words.length>=0)
-{
-   //words[1]+"##"+
-context.write(new DoubleWritable(Double.parseDouble(words[1])),new Text(words[0]));
-   
+           if(words.length>=0)
+           {
+            context.write(new DoubleWritable(Double.parseDouble(words[1])),new Text(words[0]));
+           }
 
-
-}
-        // Text currentWord  = new Text();
-
-      /*   for ( String word  : WORD_BOUNDARY .split(line)) {
-            if (word.isEmpty()) {
-               continue;
-            }
-           // currentWord  = new Text(word+"#####"+((FileSplit)context.getInputSplit()).getPath().getName()+"#####"+String.valueOf(input_size));
-          
-         }  */
       }
    }
 
@@ -117,22 +83,13 @@ context.write(new DoubleWritable(Double.parseDouble(words[1])),new Text(words[0]
       @Override 
       public void reduce(DoubleWritable word,  Iterable<Text> values,  Context context)
          throws IOException,  InterruptedException {
-//String key=word.toString();
-//String key_value[]=key.split("##");
 
-//if(key_value.length>0)
-//{
-Text count=new Text();
-       for(Text rank:values)
-{
-//count=rank;
-context.write(rank,word);
-}
-//new Text(String.valueOf(key_value[0]))
-
-//}
-        
-         
+          Text count=new Text();
+          for(Text rank:values)
+           {
+            context.write(rank,word);
+           }
+       
       }
    }
 }
